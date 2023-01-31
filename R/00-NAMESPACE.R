2dcac34b8738893588e2c13bad6785d1df7d41ed
Antonia Runge <antonia.runge@inwt-statistics.de>
2023-01-31 15:11:05 +0100

    Version 23.01.1: apply importData module from DataTools (#17)
    * apply importData module from IsoAppTools
    
    * sidebars with auto-scroll
    
    * add news.md
    
    * update description
    
    * remove dependencies
    
    * use new package name

--- R/00-NAMESPACE.R
+++ R/00-NAMESPACE.R
@@ -6,20 +6,18 @@
 #' @importFrom dplyr ungroup summarise group_by "%>%" summarise_ group_by_
 #' @importFrom ggplot2 aes_string element_text geom_errorbar geom_point ggplot
 #' labs scale_x_discrete theme aes aes_ geom_boxplot ylab xlab
-#' @importFrom shinyjs reset useShinyjs alert
+#' @importFrom DataTools importDataUI importDataServer
+#' @importFrom shinyjs useShinyjs alert
 #' @importFrom shinyWidgets pickerInput updatePickerInput
-#' @importFrom openxlsx read.xlsx write.xlsx
+#' @importFrom openxlsx write.xlsx
 #' @importFrom graphics plot boxplot
 #' @importFrom pROC plot.roc
 #' @importFrom Rcpp evalCpp
 #' @importFrom rstan extract
-#' @importFrom readr guess_encoding
-#' @importFrom readxl read_excel
 #' @importFrom stats lm as.formula predict rexp rnorm terms rpois median quantile sd qnorm chisq.test cor lm.fit model.matrix na.omit
-#' @importFrom utils head read.csv
 #' @importFrom jsonlite toJSON
 #' @importFrom grDevices dev.off pdf png svg tiff
-#' @importFrom utils capture.output write.table
+#' @importFrom utils capture.output head read.csv write.table
 #' @importFrom tidyr gather
 #' @importFrom BMSC prepModelNames getModelFits plotModelFit
 NULL

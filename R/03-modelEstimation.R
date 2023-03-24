0719ba4c44cdf73240b46c6a276139007698f92a
Antonia Runge <antonia.runge@inwt-statistics.de>
2023-03-24 12:16:13 +0100

    refactoring: extract preparation of data for the model

--- R/03-modelEstimation.R
+++ R/03-modelEstimation.R
@@ -126,7 +126,12 @@ modelEstimation <- function(input, output, session, data) {
   })
 
   # MODEL DOWN- / UPLOAD ----
-  downloadModelServer("modelDownload", data = data, inputs = input, model = m)
+  # no download of model output since it is too large to be uploadad again
+  downloadModelServer("modelDownload",
+                      data = data, 
+                      inputs = input, 
+                      #model = rawModel,
+                      model = NULL)
   
   uploadedData <- uploadModelServer("modelUpload")
   
@@ -148,136 +153,79 @@ modelEstimation <- function(input, output, session, data) {
   }) %>%
     bindEvent(uploadedData$inputs)
   
-  #observeEvent(uploadedData$model, priority = -100, {
-    #browser()
-    # update model and notes in tab "Estimates" ----
-    #m(uploadedData$model)
-  #})
+  # # currently model upload is not possible since the model output is to large to be uploaded again
+  # # upload limit exceeded
+  #
+  # observeEvent(uploadedData$model, priority = -200, {
+  # ## update model ----
+  #   rawModel(uploadedData$model)
+  # })
   
   # RUN MODEL ----
-  m <- eventReactive(input$run, {
-    if(is.null(input$y) || input$y == ""){
-      shinyjs::alert("Please select an dependent variable")
-      return(NULL)
-    }
+  dataModel <- reactiveVal()
+  rawModel <- reactiveVal()
+  
+  observe({
+    prepData <- data() %>%
+      prepareData(in_x = input$x,
+                  in_xUnc = input$xUnc,
+                  in_y = input$y,
+                  in_yUnc = input$yUnc,
+                  in_xCategorical = input$xCategorical,
+                  in_xCatUnc = input$xCatUnc,
+                  in_regType = input$regType) %>%
+      tryCatchWithWarningsAndErrors()
     
-    if(is.null(input$x) || all(input$x == "")){
-      shinyjs::alert("Please select an independent variable(s)")
-      return(NULL)
-    }
-    xVars <- input$x
-    if(!is.null(input$xCategorical) && any(input$xCategorical != "")){
-      xVars <- c(xVars, input$xCategorical)
-      xCategorical <- input$xCategorical
-    } else {
-      xCategorical <- ""
-    }
-    FORMULA <- generateFormula(input$y, xVars)
-    dataModel <- data()
-
-    if(all(is.na(dataModel[, input$y]))){
-      shinyjs::alert("Dependent variable has no numeric values")
-      return(NULL)
-    }
-    
-    if(any(apply(dataModel[, input$x, drop = FALSE], 2, function(k) all(is.na(as.numeric(k)))))){
-      shinyjs::alert("At least one x variable has no numeric values")
-      return(NULL)
-    }
-    
-    dataModel[, input$x] <- as.data.frame(sapply(dataModel[, input$x], as.numeric)) 
-
-    if(!is.null(input$xUnc)){
-      xUnc <- dataModel[, input$xUnc, drop = FALSE]
-      names(xUnc) <- input$x
-      if(any(apply(dataModel[, input$xUnc, drop = FALSE], 2, function(k) all(as.numeric(is.na(k)))))){
-        shinyjs::alert("At least one x uncertainty variable has no numeric values")
-        return(NULL)
+    if (is.null(prepData)) {
+      dataModel(NULL)
+      rawModel(NULL)
       }
-      if(length(input$xUnc) != length(input$x)){
-        shinyjs::alert("x-uncertainty variables must have same length as x-variables")
-        return(NULL)
-      }
-    } else {
-      xUnc <- NULL
-    }
     
-    if(!is.null(input$xCatUnc)){
-      xCatUnc <- dataModel[, input$xCatUnc, drop = FALSE]
-      names(xCatUnc) <- input$xCategorical
-      if(any(apply(dataModel[, input$xCatUnc, drop = FALSE], 2, function(k) all(as.numeric(is.na(k)))))){
-        shinyjs::alert("At least one x categorical uncertainty variable has no numeric values")
-        return(NULL)
-      }
-      if(length(input$xCatUnc) != length(input$xCategorical)){
-        shinyjs::alert("x-missclassification variables must have same length as x-categorical variables")
-        return(NULL)
-      }
-      
-    } else {
-      xCatUnc <- NULL
-    }
-    if(xCategorical[1] == ""){
-      mVars <- c(input$x)
-    } else {
-      mVars <- c(input$x, xCategorical)
-    }
-    missingVars <- names(which(sapply(mVars, function(x) any(is.na(dataModel[, x])))))
-    if(length(missingVars) > 0){
-      dataModel2 <- dataModel
-      dataModel <- na.omit(dataModel)
-      nMissing <- nrow(dataModel2) - nrow(dataModel)
-      shinyjs::alert(paste0("Missing values found in following variables: ", paste(missingVars, collapse = ", "), "; ", nMissing, " observations deleted."))
-    }
-
-    if(!is.numeric(dataModel[, input$y])){
-      dataModel[, input$y] <- as.numeric(dataModel[, input$y])
-    }
+    req(prepData)
+    FORMULA <- generateFormula(input$y, prepData$xVars)
     
-    if(!is.null(input$yUnc)){
-      yUnc <- as.numeric(as.vector(dataModel[, input$yUnc]))
-      if(all(is.na(yUnc))){
-        shinyjs::alert("Dependent variable uncertainty has no numeric values")
-        return(NULL)
-      }
-    } else {
-      yUnc <- rep(0, nrow(dataModel))
-    }
-    
-    if(input$regType == "logistic" & any(!(dataModel[, input$y] %in% c(0,1)))){
-      shinyjs::alert("Dependent variable must have only 0 and 1 values if logistic regression is selected.")
-      return(NULL)
-    }
     set.seed(1234)
-    
-    model <- withProgress({constrSelEst(
-      formula = FORMULA,
-      mustInclude = input$mustInclude, 
-      mustExclude = input$mustExclude,
-      maxExponent = input$maxExp,
-      inverseExponent = input$inverseExp,
-      interactionDepth = input$interactionDepth,
-      categorical = xCategorical,
-      ar1 = input$ar1,
-      intercept = input$intercept,
-      constraint_1 = input$constraint, data = dataModel,
-      xUncertainty = xUnc,
-      xCatUncertainty = xCatUnc,
-      yUncertainty = yUnc, maxNumTerms = input$maxTerms,
-      type = input$regType,
-      scale = input$scale,
-      chains = input$nChains,
-      burnin = input$burnin,
-      iterations = input$iter,
-      shiny = TRUE,
-      imputeMissings = input$imputeMissings) %>%
+    model <- withProgress({
+      constrSelEst(
+        formula = FORMULA,
+        mustInclude = input$mustInclude,
+        mustExclude = input$mustExclude,
+        maxExponent = input$maxExp,
+        inverseExponent = input$inverseExp,
+        interactionDepth = input$interactionDepth,
+        categorical = prepData$xCategorical,
+        ar1 = input$ar1,
+        intercept = input$intercept,
+        constraint_1 = input$constraint,
+        data = prepData$dataModel,
+        xUncertainty = prepData$xUnc,
+        xCatUncertainty = prepData$xCatUnc,
+        yUncertainty = prepData$yUnc,
+        maxNumTerms = input$maxTerms,
+        type = input$regType,
+        scale = input$scale,
+        chains = input$nChains,
+        burnin = input$burnin,
+        iterations = input$iter,
+        shiny = TRUE,
+        imputeMissings = input$imputeMissings
+      ) %>%
         tryCatchWithWarningsAndErrors()
-    }, 
-    value = 0, 
+    },
+    value = 0,
     message = "Calculation in progess",
     detail = 'This may take a while')
     
-    if (is.null(model)) return(NULL)
+    dataModel(prepData$dataModel)
+    rawModel(model)
+  }) %>%
+    bindEvent(input$run)
+  
+  m <- eventReactive(rawModel(), ignoreNULL = FALSE, ignoreInit = TRUE, { 
+    dataModel <- dataModel()
+    model <- rawModel()
+    
+    if (is.null(dataModel) || is.null(model)) return(NULL)
     
     names(model$models) <- prepModelNames(model$models)
     # if(any(sapply(1:length(model), function(x) is.null(model[[x]])))){

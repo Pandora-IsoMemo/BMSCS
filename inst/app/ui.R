497fe1bb383e210b962d5103e056daae5710de2a
Antonia Runge <antonia.runge@inwt-statistics.de>
2025-03-26 17:40:40 +0100

    BMSCS 25.03.0: update links, readme, buildignore (#41)
    * update links, readme, buildignore
    
    * update news.md
    
    * update news.md
    
    * update custom.css

--- inst/app/ui.R
+++ inst/app/ui.R
@@ -3,32 +3,12 @@ library("BMSCS")
 shiny::tagList(
   shinyjs::useShinyjs(),
   shiny::navbarPage(
-    includeCSS("www/custom.css"),
+    header = includeCSS("www/custom.css"),
     title = paste("BMSC App", utils::packageVersion("BMSCS")),
     theme = shinythemes::shinytheme("flatly"),
     id = "tabs",
     dataInputUI("data", "Data Input"),
     modelEstimationUI("model", "Model Input")
   ),
-  div(
-    id = "header-right",
-    div(
-      id = "logo-mpi",
-      tags$a(href = "https://www.mpg.de/en",
-             img(src = "MPIlogo.png", alt = "Supported by the Max Planck society"),
-             target = "_blank"
-      )
-    ),
-    div(
-      id = "logo-isomemo",
-      tags$a(href = "https://isomemo.com/",
-             img(src = "IsoMemoLogo.png", alt = "IsoMemo"),
-             target = "_blank"
-      )
-    ),
-    div(
-      id = "help",
-      actionButton("getHelp", "Help")
-    )
-  )
+  shinyTools::headerButtonsUI(id = "header", help_link = "https://pandora-isomemo.github.io/BMSCS/")
 )

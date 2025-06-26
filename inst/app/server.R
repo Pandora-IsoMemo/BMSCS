860c411bd60cdd2c8b445e2f40eced42484d766e
Antonia Runge <antonia.runge@inwt-statistics.de>
2025-06-26 13:30:07 +0200

    remove deprecated argument

--- inst/app/server.R
+++ inst/app/server.R
@@ -8,12 +8,4 @@ function(input, output, session) {
   options("scipen"=100, "digits"=4)
   data <- shiny::callModule(dataInput, "data")
   shiny::callModule(modelEstimation, "model", data)
-  
-  observeEvent(input$getHelp, {
-    showModal(modalDialog(
-      title = "Help",
-      easyClose = TRUE,
-      getHelp(input$tab)
-    ))
-  })
 }
